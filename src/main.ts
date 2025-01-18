4dad290abdd3d9c1b599f2da95224be4ebb94015
Gabriel SIGNE <144789476+Gabriel-SIGNE@users.noreply.github.com>
2025-01-18 13:40:54 +0100

    MISE A JOUR DU PORT

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, documentFactory);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
